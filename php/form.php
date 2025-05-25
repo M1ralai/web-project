afb27f1d6392132d6bebae1e8abb9fd95f8535c4
M1ralai <atahan.dmrr@gmail.com>
2025-05-25 21:54:50 +0300

    php hataları düzeltildi

--- php/form.php
+++ php/form.php
@@ -1,13 +1,14 @@
-<?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): {
     $isimsoyisim = htmlspecialchars(trim($_POST['isimsoyisim'] ?? ''));
     $email = htmlspecialchars(trim($_POST['email'] ?? ''));
     $baslik = htmlspecialchars(trim($_POST['baslik'] ?? ''));
     $mesaj = htmlspecialchars(trim($_POST['mesaj'] ?? ''));
-    echo "<p class="paragraph">Ad Soyad: $isimsoyisim</p>";
-    echo "<p class="paragraph">E-posta: $email</p>";
-    echo "<p class="paragraph">Baslik: $baslik</p>";
-    echo "<p class="paragraph">Mesaj: $mesaj</p>";
-    echo "<p class="paragraph"> $mesaj</p>";
-    echo "<a href="index.html">geri dön</a>";
-}
+    if ($isimsoyisim && $email && $baslik && $mesaj) {
+    echo"<p>Verdiğin isim soyisim: $isimsoyisim</p><br>";
+    echo"<p>Verdiğin e mail: $email</p><br>";
+    echo"<p>Verdiğin baslik: $baslik</p><br>";
+    echo"<p>Verdiğin mesaj: $mesaj</p>";
+    echo"<a href='../index.html'>geri dön</a>";
+    }
+}endif;
 ?>
